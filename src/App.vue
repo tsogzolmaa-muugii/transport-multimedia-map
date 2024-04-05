d040c9f7377e5cafb33e0e5b26a441c576da2060
Tsogzolmaa Munkhbat <tsogzolmaamonkhbat@gmail.com>
2024-04-05 19:44:43 +0200

    Add example

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,85 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <form>
+    <label for="zoom">Zoom:</label>
+    <input type="number" id="zoom" v-model="zoom" />
+  </form>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
+  <ol-map style="height: 400px">
+    <ol-view
+      ref="view"
+      :center="center"
+      :rotation="rotation"
+      :zoom="zoom"
+      :projection="projection"
+      @change:center="centerChanged"
+      @change:resolution="resolutionChanged"
+      @change:rotation="rotationChanged"
+    />
 
-  <main>
-    <TheWelcome />
-  </main>
+    <ol-tile-layer>
+      <ol-source-osm />
+    </ol-tile-layer>
+
+    <ol-rotate-control></ol-rotate-control>
+    <ol-interaction-link />
+  </ol-map>
+
+  <ul>
+    <li>center : {{ currentCenter }}</li>
+    <li>resolution : {{ currentResolution }}</li>
+    <li>zoom : {{ currentZoom }}</li>
+    <li>rotation : {{ currentRotation }}</li>
+  </ul>
 </template>
 
+<script setup>
+import { ref } from 'vue'
+
+const center = ref([40, 40])
+const projection = ref('EPSG:4326')
+const zoom = ref(8)
+const rotation = ref(0)
+
+const currentCenter = ref(center.value)
+const currentZoom = ref(zoom.value)
+const currentRotation = ref(rotation.value)
+const currentResolution = ref(0)
+
+function resolutionChanged(event) {
+  currentResolution.value = event.target.getResolution()
+  currentZoom.value = event.target.getZoom()
+}
+function centerChanged(event) {
+  currentCenter.value = event.target.getCenter()
+}
+function rotationChanged(event) {
+  currentRotation.value = event.target.getRotation()
+}
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
+.ol-map {
+  position: relative;
+}
+.ol-map-loading:after {
+  content: '';
+  box-sizing: border-box;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 80px;
+  height: 80px;
+  margin-top: -40px;
+  margin-left: -40px;
+  border-radius: 50%;
+  border: 5px solid rgba(180, 180, 180, 0.6);
+  border-top-color: var(--vp-c-brand-1);
+  animation: spinner 0.6s linear infinite;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
+@keyframes spinner {
+  to {
+    transform: rotate(360deg);
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import OpenLayersMap from 'vue3-openlayers'
+import 'vue3-openlayers/styles.css' // vue3-openlayers version < 1.0.0-*
+
+const app = createApp(App)
+app.use(OpenLayersMap /* options */)
+
+app.mount('#app')
